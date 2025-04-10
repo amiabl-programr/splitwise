b78a79c28e8f06ddc57ff9404e3da5ca8b452aa4
Akukwe Ebube <103237255+EbubeAkukwe@users.noreply.github.com>
2025-04-10 15:29:54 +0100

    feat: Update authRoutes.js
    Added the delete group end point (/groups/:groupId) which takes the group id to delete and checks if the owner id in the group details is the same as the user id making the request

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {login, signup, logout}  from '../controllers/authController.js';
-import { createGroups, getAllGroups} from '../controllers/groupController.js'
+import { createGroups, getAllGroups, deleteGroup} from '../controllers/groupController.js'
 import verifyAuthToken from '../middleware/verifyAuthToken.js';
 import verifySessionToken from "../middleware/verifySessionToken.js";
 
@@ -10,6 +10,7 @@ router.post("/login",verifyAuthToken, login);
 router.post("/signup", signup);
 router.post("/logout", logout);
 router.post("/create-group", verifySessionToken, createGroups);
+router.delete("/groups/:groupId", verifySessionToken, deleteGroup);
 router.get("/groups", verifySessionToken, getAllGroups);
 
 
