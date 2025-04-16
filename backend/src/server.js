c51f7e914c4a753675ed7703464a63c4fcb58723
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-04-16 19:12:48 +0100

    fix: add vercel domain to allowed urls by cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,10 +7,20 @@ import authRoutes from "./routes/authRoutes.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = ['http://localhost:5173', 'https://vercel.app'];
+
 app.use(cors({
-   origin: 'http://localhost:5173', // or your frontend URL
-   credentials: true
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
+
 
 app.use("/api", authRoutes);
 
