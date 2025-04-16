e5cc38e6f593db80d622b1034c98d6b005fb9f92
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-04-16 18:55:25 +0100

    fix: change firebase config

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,12 +1,18 @@
 import admin from "firebase-admin";
+import fs from "fs";
+import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables
 
-const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_JSON, 'base64').toString('utf-8'));
+// Path to the service account file
+const serviceAccountPath = path.resolve(process.env.SERVICE_ACCOUNT_PATH);
+
+// Load and parse the service account JSON file
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 // Initialize Firebase Admin SDK
-if (!admin.apps.length) {
+if (!admin.apps.length) {   
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
     });
